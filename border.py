d5dca9142dc79de642cb778b33ab6448383d340e
TEE MS van der Linde <jattie.VanDerLinde@europe.tel.com>
2018-03-20 12:28:55 +0000

    new sample better sorted

--- border.py
+++ border.py
@@ -8,9 +8,10 @@ from os import path as osp
 # 
 posterizeval = 2 #default: 3
 coloursort = False # True or False - sort by frequency
-border1 = 5
+border1 = 7
 border2 = 15
 sampledir = 'thm'
+tnsize=(150,150)
 
 # https://www.reddit.com/r/Python/comments/oixmu/add_entry_to_windows_7_context_menu_that_runs/
 def Mbox(title, text, style):
@@ -18,25 +19,27 @@ def Mbox(title, text, style):
 
 #pic=img.open(r'C:\Users\Jattie\Pictures\lionhead2.JPG')
 pic=img.open(r'lionhead2.JPG')
-pic2 = pic.resize((100,100))
-w,h=pic2.size
-pic2=opts.posterize(pic2,posterizeval)
-pix=pic2.getcolors()
+tn=pic # copy the pic th thumbnail
+tn.thumbnail(tnsize) # reduce thumbnail to tnsize
+#pic2 = pic.resize((100,100))
+#w,h=tn.size
+tnp=opts.posterize(tn,posterizeval)
+pix=tnp.getcolors()
 print('colours: ',len(pix))
-ColArr=[]
-for i in range(len(pix)):
-    RGBint=(pix[i][1][0]<<16) + (pix[i][1][1]<<8) + pix[i][1][2]
-    ColArr.append([pix[i][0],RGBint])
-if coloursort == True:
+ColArr=[] # a new value to store the converted rgb values
+for i in range(len(pix)): # loop through all the colours
+    RGBint=(pix[i][1][0]<<16) + (pix[i][1][1]<<8) + pix[i][1][2] # convert tuple to rgb
+    ColArr.append([pix[i][0],RGBint]) # append conversions
+if coloursort == True: # sort if selected
     ColArr.sort(reverse=True)
 
 #V1=int(len(ColArr)*0.9)
 #V2=int(len(ColArr)*0.01)
 for i in range(len(ColArr)):
     for j in range(len(ColArr)):
-        pic2 = pic.resize((100,100))
-        pic2=opts.expand(pic2,border=border1,fill=ColArr[i][1])
-        pic2=opts.expand(pic2,border=border2,fill=ColArr[j][1])
+        pic2 = tn
+        pic2=opts.expand(pic2,border=border1,fill=ColArr[j][1])
+        pic2=opts.expand(pic2,border=border2,fill=ColArr[i][1])
         filename='thumb_{:02d}_{:02d}.jpg'.format(i,j)
         mkd(osp.dirname('%s\\%s' % (sampledir,filename)), exist_ok=True)
         filepath=r'{}\{}'.format(sampledir,filename)

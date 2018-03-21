8d94b442073fd696a9d6c7f640f48e1fde8ba0c3
TEE MS van der Linde <jattie.VanDerLinde@europe.tel.com>
2018-03-21 14:10:12 +0000

    add right click script

---
+++ right_click.py
@@ -0,0 +1,26 @@
+import ctypes
+import sys
+import border as b
+import os
+
+try:
+    sys.argv[1:]
+except NameError:
+    argc=0
+else:
+    argc=len(sys.argv)
+if argc<=1:
+    ctypes.windll.user32.MessageBoxW(0, 'Error, a filename is required!' ,'Error', 0x30)
+    exit(-1)
+elif argc==2: # a single parameter
+    filepath=sys.argv[1]
+
+cdir=os.path.dirname(filepath)
+b.sampledir=cdir+r'\thm'
+
+#https://stackoverflow.com/questions/34840838/how-to-specify-what-actually-happens-when-yes-no-is-clicked-with-ctypes-messageb
+
+#ctypes.windll.user32.MessageBoxW(0, 'Filename: {}'.format(filepath),'Python Photo Frame Script', 0x40)
+
+tn, pic, border1, border2, ColArr = b.init(sys.argv[1])
+b.maketh(tn, border1, border2, ColArr)
